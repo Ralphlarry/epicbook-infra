f47a44a25e6958625ee1f533b98e4633b45ac2bf
larryawesome <ibtclass00@gmail.com>
2026-04-19 10:39:47 +0100

    SSH key edited in terraform

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -55,7 +55,7 @@ resource "azurerm_linux_virtual_machine" "frontend_vm" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("${path.module}/id_rsa.pub")
+    public_key = file("${path.module}/epicbook_key.pub")
   }
 
   source_image_reference {
